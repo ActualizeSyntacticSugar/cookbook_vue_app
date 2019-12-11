c3b4a932d5a6c139ec865822ed36e0a6e0430884
Peter Jang <peter.x.jang@gmail.com>
2019-12-10 18:58:48 -0600

    Show all recipes from recipes index web request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="recipe in recipes">
+      <h3>{{ recipe.title }}</h3>
+      <p>Ingredients: {{ recipe.ingredients }}</p>
+      <p>Directions: {{ recipe.directions }}</p>
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!!!"
+      message: "Welcome to Vue.js!!!",
+      recipes: []
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("/api/recipes").then(response => {
+      this.recipes = response.data;
+      console.log(this.recipes);
+    });
+  },
   methods: {}
 };
 </script>
