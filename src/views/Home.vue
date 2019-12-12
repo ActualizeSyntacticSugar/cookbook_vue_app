6a152e939b9cae7dabb5f7c05259459633a35a81
Peter Jang <peter.x.jang@gmail.com>
2019-12-11 18:25:36 -0600

    Update a recipe using recipes update web request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,22 @@
         <img v-bind:src="recipe.image_url" alt="" />
         <p>Ingredients: {{ recipe.ingredients }}</p>
         <p>Directions: {{ recipe.directions }}</p>
+        <h4>Edit Recipe</h4>
+        <div>
+          Title:
+          <input type="text" v-model="recipe.title" />
+          Chef:
+          <input type="text" v-model="recipe.chef" />
+          Ingredients:
+          <input type="text" v-model="recipe.ingredients" />
+          Directions:
+          <input type="text" v-model="recipe.directions" />
+          Prep time:
+          <input type="text" v-model="recipe.prep_time" />
+          Image url:
+          <input type="text" v-model="recipe.image_url" />
+          <button v-on:click="updateRecipe(recipe)">Update</button>
+        </div>
       </div>
     </div>
 
@@ -84,6 +100,20 @@ export default {
       } else {
         this.currentRecipe = recipe;
       }
+    },
+    updateRecipe: function(recipe) {
+      console.log("updateRecipe...", recipe);
+      var params = {
+        input_title: recipe.title,
+        input_chef: recipe.chef,
+        input_prep_time: recipe.prep_time,
+        input_ingredients: recipe.ingredients,
+        input_directions: recipe.directions,
+        input_image_url: recipe.image_url
+      };
+      axios.patch("/api/recipes/" + recipe.id, params).then(response => {
+        console.log(response);
+      });
     }
   }
 };
