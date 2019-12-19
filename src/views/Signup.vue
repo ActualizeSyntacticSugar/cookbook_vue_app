6e37fec64ec858a49b0cd91d2626cf040ad409a0
Peter Jang <peter.x.jang@gmail.com>
2019-12-18 20:08:31 -0600

    Add frontend validations and cat status image to signup page

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -4,11 +4,13 @@
       <form v-on:submit.prevent="submit()">
         <h1>Signup</h1>
         <ul>
+          <img v-if="status" v-bind:src="'https://http.cat/' + status" alt="" />
           <li class="text-danger" v-for="error in errors">{{ error }}</li>
         </ul>
         <div class="form-group">
           <label>Name:</label>
           <input type="text" class="form-control" v-model="name" />
+          <p class="text-primary">{{ 50 - name.length }} characters remaining</p>
         </div>
         <div class="form-group">
           <label>Email:</label>
@@ -17,10 +19,12 @@
         <div class="form-group">
           <label>Password:</label>
           <input type="password" class="form-control" v-model="password" />
+          <p v-if="password.length > 0 && password.length < 8" class="text-danger">Must be at least 8 characters!</p>
         </div>
         <div class="form-group">
           <label>Password confirmation:</label>
           <input type="password" class="form-control" v-model="passwordConfirmation" />
+          <p v-if="password !== passwordConfirmation" class="text-danger">Passwords must match!</p>
         </div>
         <input type="submit" class="btn btn-primary" value="Submit" />
       </form>
@@ -38,7 +42,8 @@ export default {
       email: "",
       password: "",
       passwordConfirmation: "",
-      errors: []
+      errors: [],
+      status: ""
     };
   },
   methods: {
@@ -56,6 +61,7 @@ export default {
         })
         .catch(error => {
           this.errors = error.response.data.errors;
+          this.status = error.response.status;
         });
     }
   }
